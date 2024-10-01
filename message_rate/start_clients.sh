a399bd8b5ecc5f3bc8b535b2eedb7e1ecc367e7b
tziegler <tobias.ziegler@cs.tu-darmstadt.de>
2024-10-01 15:42:47 +0200

    ip as argument

--- message_rate/start_clients.sh
+++ message_rate/start_clients.sh
@@ -1,5 +1,5 @@
 N=$1
 for ((n = 0; n < N; n++)); do
     core_id=$((n + 3))
-    numactl -C $core_id ./sockperf/sockperf tp -p $((50000 + n)) -i 172.31.6.255 -m 1472 -t 1000 &
+    numactl -C $core_id ./sockperf/sockperf tp -p $((50000 + n)) -i $2 -m 1472 -t 1000 &
 done
